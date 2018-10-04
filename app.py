8fe65bab0675a96fce255ea3c629ba00c87c4bb1
Ollymid <ollymiddleton2@gmail.com>
2018-10-04 20:34:52 +0100

    submit-ideas

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.contrib.cache import SimpleCache
+
 
 app = Flask(__name__)
+cache = SimpleCache()
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+	ideas = cache.get('ideas') or []
+	ideas_list = ''
+	return f"{ideas}<br><form action=/submit method=post><input name=idea></form>"	
 
 
-@app.route('/first_round_voting')
-def hello():
-    return 'Hello, World'
+@app.route('/submit', methods=['POST'])
+def submit_idea():
+	ideas = cache.get('ideas') or []
+	idea = request.form['idea']
+	ideas.append(idea)
+	cache.set('ideas', ideas, timeout=0)
+	return 'success'
 
-@app.route('/first_round_voting')
-def hello():
-    return 'Hello, World'
+
+# @app.route('/first_round_voting')
+# def hello():
+#     return 'Hello, World'
+
+# @app.route('/second_round_voting')
+# def hello():
+#     return 'Hello, World'
