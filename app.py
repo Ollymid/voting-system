d2072bd9896ef10ebe3194622742d5184eacf641
Ollymid <ollymiddleton2@gmail.com>
2018-10-04 20:04:51 +0100

    added some routes

--- app.py
+++ app.py
@@ -3,5 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello, World!'
+
+
+@app.route('/first_round_voting')
+def hello():
+    return 'Hello, World'
+
+@app.route('/first_round_voting')
+def hello():
+    return 'Hello, World'
